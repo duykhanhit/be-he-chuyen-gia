3d3980c420dbe55e28d6e652bd702491b971a457
Hoàng Duy Khánh <hdkhanhit@gmai.com>
2021-12-05 08:43:38 +0700

    add router

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,12 @@
 const errorHandle = require("../middlewares/errorHandle");
 const cauhinhRouter = require("./cauhinhRouter");
+const laptopRouter = require("./laptopRouter");
+const ruleRouter = require("./ruleRouter");
 
 module.exports = (app) => {
   app.use("/api", cauhinhRouter);
+  app.use("/api", laptopRouter);
+  app.use("/api", ruleRouter);
 
   // Middleware handle error
   app.use(errorHandle);
